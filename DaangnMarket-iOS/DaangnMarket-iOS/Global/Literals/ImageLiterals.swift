c14d8628b003ce211992efa3042b78dafe8003e8
L-j-h-c <ckrgkswnsgh@naver.com>
2022-05-16 16:09:28 +0900

    [Chore] 상수 추가

---
+++ DaangnMarket-iOS/DaangnMarket-iOS/Global/Literals/ImageLiterals.swift
@@ -0,0 +1,27 @@
+//
+//  ImageLiterals.swift
+//  DaangnMarket-iOS
+//
+//  Created by Junho Lee on 2022/05/16.
+//
+
+import UIKit
+
+struct ImageLiterals{
+    
+    struct MainTabBar{
+        static let homeIcon = UIImage(named: "icon_home")!
+    }
+    
+    struct PostList {
+        
+    }
+    
+    struct PostDetail {
+        
+    }
+    
+    struct PostWrite {
+        
+    }
+}
