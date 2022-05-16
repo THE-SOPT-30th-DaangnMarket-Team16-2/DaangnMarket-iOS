e0ef1efabf921f536ee53c6d026edecba263fcd5
L-j-h-c <ckrgkswnsgh@naver.com>
2022-05-16 20:05:48 +0900

    [Add] Color Extension 등록

--- DaangnMarket-iOS/DaangnMarket-iOS/Global/Extension/Design/UIColor+.swift
+++ DaangnMarket-iOS/DaangnMarket-iOS/Global/Extension/Design/UIColor+.swift
@@ -9,91 +9,47 @@ import UIKit
 
 extension UIColor {
     
-    @nonobjc class var white: UIColor {
+    @nonobjc class var carrotWhite: UIColor {
         return UIColor(white: 1.0, alpha: 1.0)
     }
     
-    @nonobjc class var nadoBlack: UIColor {
-        return UIColor(red: 0.0, green: 29.0 / 255.0, blue: 25.0 / 255.0, alpha: 1.0)
+    @nonobjc class var carrotBlack: UIColor {
+        return UIColor(red: 27.0 / 255.0, green: 26.0 / 255.0, blue: 31.0 / 255.0, alpha: 1.0)
     }
     
-    @nonobjc class var mainBlack: UIColor {
-        return UIColor(red: 0.0, green: 28.0 / 255.0, blue: 24.0 / 255.0, alpha: 1.0)
+    @nonobjc class var carrotButtonOrange: UIColor {
+        return UIColor(red: 254.0 / 126.0, green: 53.0 / 255.0, blue: 1.0 / 255.0, alpha: 1.0)
     }
     
-    @nonobjc class var gray0: UIColor {
-        return UIColor(red: 246.0 / 255.0, green: 246.0 / 255.0, blue: 249.0 / 255.0, alpha: 1.0)
+    @nonobjc class var carrotTextOrange: UIColor {
+        return UIColor(red: 241.0 / 255.0, green: 132.0 / 255.0, blue: 79.0 / 255.0, alpha: 1.0)
     }
     
-    @nonobjc class var gray1: UIColor {
-        return UIColor(red: 232.0 / 255.0, green: 232.0 / 255.0, blue: 235.0 / 255.0, alpha: 1.0)
+    @nonobjc class var carrotBlue: UIColor {
+        return UIColor(red: 21.0 / 255.0, green: 105.0 / 255.0, blue: 184.0 / 255.0, alpha: 1.0)
     }
     
-    @nonobjc class var gray2: UIColor {
-        return UIColor(red: 192.0 / 255.0, green: 192.0 / 255.0, blue: 203.0 / 255.0, alpha: 1.0)
+    @nonobjc class var carrotDeepGray: UIColor {
+        return UIColor(red: 49.0 / 255.0, green: 49.0 / 255.0, blue: 49.0 / 255.0, alpha: 1.0)
     }
     
-    @nonobjc class var mainDark: UIColor {
-        return UIColor(red: 5.0 / 255.0, green: 161.0 / 255.0, blue: 143.0 / 255.0, alpha: 1.0)
+    @nonobjc class var carrotDarkLightGray: UIColor {
+        return UIColor(red: 132.0 / 255.0, green: 136.0 / 255.0, blue: 146.0 / 255.0, alpha: 1.0)
     }
     
-    @nonobjc class var mainLight: UIColor {
-        return UIColor(red: 223.0 / 255.0, green: 246.0 / 255.0, blue: 244.0 / 255.0, alpha: 1.0)
+    @nonobjc class var carrotLineGray: UIColor {
+        return UIColor(red: 169.0 / 255.0, green: 169.0 / 255.0, blue: 169.0 / 255.0, alpha: 1.0)
     }
     
-    @nonobjc class var gray4: UIColor {
-        return UIColor(red: 86.0 / 255.0, green: 86.0 / 255.0, blue: 95.0 / 255.0, alpha: 1.0)
+    @nonobjc class var carrotSquareGray: UIColor {
+        return UIColor(red: 226.0 / 255.0, green: 226.0 / 255.0, blue: 228.0 / 255.0, alpha: 1.0)
     }
     
-    @nonobjc class var mainText: UIColor {
-        return UIColor(white: 60.0 / 255.0, alpha: 1.0)
+    @nonobjc class var carrotSquareLightGray: UIColor {
+        return UIColor(red: 227.0 / 255.0, green: 226.0 / 255.0, blue: 231.0 / 255.0, alpha: 1.0)
     }
     
-    @nonobjc class var gray3: UIColor {
-        return UIColor(red: 148.0 / 255.0, green: 149.0 / 255.0, blue: 158.0 / 255.0, alpha: 1.0)
-    }
-    
-    @nonobjc class var mainDefault: UIColor {
-        return UIColor(red: 0.0, green: 200.0 / 255.0, blue: 176.0 / 255.0, alpha: 1.0)
-    }
-    
-    @nonobjc class var shadowDefault: UIColor {
-        return UIColor(red: 229.0 / 255.0, green: 229.0 / 255.0, blue: 232.0 / 255.0, alpha: 1.0)
-    }
-    
-    @nonobjc class var paleGray: UIColor {
-        return UIColor(red: 251.0 / 255.0, green: 251.0 / 255.0, blue: 253.0 / 255.0, alpha: 1.0)
-    }
-    
-    @nonobjc class var red: UIColor {
-        return UIColor(red: 255.0 / 255.0, green: 76.0 / 255.0, blue: 64.0 / 255.0, alpha: 1.0)
-    }
-
-    @nonobjc class var bgGray: UIColor {
-        return UIColor(red: 248.0 / 255.0, green: 249.0 / 255.0, blue: 253.0 / 255.0, alpha: 1.0)
-    }
-    
-    @nonobjc class var scrollMint: UIColor {
-        return UIColor(red: 187.0 / 255.0, green: 242.0 / 255.0, blue: 236.0 / 255.0, alpha: 1.0)
-    }
-    
-    @nonobjc class var separatorGray: UIColor {
-        return UIColor(red: 234.0 / 255.0, green: 235.0 / 255.0, blue: 241.0 / 255.0, alpha: 1.0)
-    }
-    
-    @nonobjc class var lightMint: UIColor {
-        return UIColor(red: 169.0 / 255.0, green: 244.0 / 255.0, blue: 235.0 / 255.0, alpha: 1.0)
-    }
-    
-    @nonobjc class var chatFill: UIColor {
-        return UIColor(red: 234.0 / 255.0, green: 250.0 / 255.0, blue: 248.0 / 255.0, alpha: 1.0)
-    }
-
-    @nonobjc class var chatStroke: UIColor {
-        return UIColor(red: 208.0 / 255.0, green: 242.0 / 255.0, blue: 238.0 / 255.0, alpha: 1.0)
-    }
-    
-    @nonobjc class var mintBgColor: UIColor {
-        return UIColor(red: 239.0 / 255.0, green: 246.0 / 255.0, blue: 246.0 / 255.0, alpha: 1.0)
+    @nonobjc class var carrotLineLightGray: UIColor {
+        return UIColor(red: 240.0 / 240.0, green: 28.0 / 240.0, blue: 1.0 / 255.0, alpha: 1.0)
     }
 }
