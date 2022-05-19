8465f328495c1f8693f4915096ebeff912c8fc99
devxsby <devxsby@gmail.com>
2022-05-20 00:21:55 +0900

    [Feat] #3- 중고거래 글쓰기 뷰 구현
    레이아웃 구성 및 샘플 사진 데이터 연결 완료

---
+++ DaangnMarket-iOS/DaangnMarket-iOS/Presentation/Home/PostWrite/Cells/AddPhotoCVC.swift
@@ -0,0 +1,84 @@
+//
+//  AddPhotoCVC.swift
+//  DaangnMarket-iOS
+//
+//  Created by subinyoon on 2022/05/18.
+//
+
+import UIKit
+import BSImagePicker
+import Photos
+
+
+protocol AddImageDelegate {
+    func didPickImagesToUpload(images: [UIImage])
+}
+
+class AddPhotoCVC: UICollectionViewCell {
+  
+  static let identifier = "AddPhotoCVC"
+  var delegate: AddImageDelegate!
+  
+  var selectedAssets: [PHAsset] = [PHAsset]()
+  var userSelectedImages: [UIImage] = [UIImage]()
+    
+  @IBOutlet weak var cameraButton: UIButton!
+  @IBOutlet weak var countLabel: UILabel!
+  
+  override func awakeFromNib() {
+      super.awakeFromNib()
+  }
+  
+  @IBAction func addBtnDidTap(_ sender: UIButton) {
+    
+    /// 기존 선택된 사진 모두 초기화
+    selectedAssets.removeAll()
+    userSelectedImages.removeAll()
+    
+    let imagePicker = ImagePickerController()
+    imagePicker.settings.selection.max = 10
+    imagePicker.settings.fetch.assets.supportedMediaTypes = [.image]
+    
+    let vc = self.window?.rootViewController
+   
+    vc?.presentImagePicker(imagePicker, select: { (asset) in
+        
+    }, deselect: { (asset) in
+
+    }, cancel: { (assets) in
+
+    }, finish: { (assets) in
+        
+        for i in 0..<assets.count {
+            self.selectedAssets.append(assets[i])
+        }
+      
+        self.convertAssetToImages()
+        self.delegate?.didPickImagesToUpload(images: self.userSelectedImages)
+    })
+  }
+  
+  func convertAssetToImages() {
+      
+      if selectedAssets.count != 0 {
+        
+          for i in 0..<selectedAssets.count {
+              
+              let imageManager = PHImageManager.default()
+              let option = PHImageRequestOptions()
+              option.isSynchronous = true
+              var thumbnail = UIImage()
+              
+              imageManager.requestImage(for: selectedAssets[i],
+                                        targetSize: CGSize(width: 200, height: 200),
+                                        contentMode: .aspectFit,
+                                        options: option) { (result, info) in thumbnail = result! }
+              
+              let data = thumbnail.jpegData(compressionQuality: 0.7)
+              let newImage = UIImage(data: data!)
+              
+              self.userSelectedImages.append(newImage! as UIImage)
+          }
+      }
+  }
+}
---
+++ DaangnMarket-iOS/DaangnMarket-iOS/Presentation/Home/PostWrite/Model/PhotoDataModel.swift
@@ -0,0 +1,18 @@
+//
+//  PhotoDataModel.swift
+//  DaangnMarket-iOS
+//
+//  Created by subinyoon on 2022/05/18.
+//
+
+import UIKit
+
+class PhotoDataModel {
+    
+    var userSelectedImages: [UIImage]
+    
+    public init() {
+        self.userSelectedImages = [UIImage]()
+    }
+    
+}
