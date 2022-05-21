b84e583cc2c6b1f42b74ee42dd5c3d4859dd1558
L-j-h-c <ckrgkswnsgh@naver.com>
2022-05-21 13:26:13 +0900

    [Feat] #10 - Alamofire 파일 세팅

---
+++ DaangnMarket-iOS/DaangnMarket-iOS/Data/Network/Environment/NetworkConstants.swift
@@ -0,0 +1,21 @@
+//
+//  NetworkConstants.swift
+//  DaangnMarket-iOS
+//
+//  Created by Junho Lee on 2022/05/21.
+//
+
+import Foundation
+
+struct URLConstants {
+    
+    // MARK: - Base URL
+    static let baseURL = "http://13.124.62.236"
+}
+
+struct NetworkEnvironment {
+    
+    // MARK: - timeOut
+    static let requestTimeOut = TimeInterval(10)
+    static let resourceTimeOut = TimeInterval(10)
+}
---
+++ DaangnMarket-iOS/DaangnMarket-iOS/Data/Network/Routers/AuthRouter.swift
@@ -0,0 +1,51 @@
+//
+//  AuthRouter.swift
+//  DaangnMarket-iOS
+//
+//  Created by Junho Lee on 2022/05/21.
+//
+
+import Foundation
+import Alamofire
+
+enum AuthRouter {
+    case requestSignUp(email: String, name: String, pw: String)
+    case requestSignIn(email: String, pw: String)
+}
+
+extension AuthRouter: BaseRouter {
+    
+    var path: String {
+        switch self {
+        case .requestSignUp:
+            return "/auth/signup"
+        case .requestSignIn:
+            return "/auth/signin"
+        }
+    }
+    
+    var method: HTTPMethod {
+        switch self {
+        case .requestSignUp, .requestSignIn:
+            return .post
+        }
+    }
+    
+    var parameters: RequestParams {
+        switch self {
+        case .requestSignUp(let email, let name, let pw):
+            let body: [String : Any] = [
+                "email": email,
+                "name": name,
+                "password": pw
+            ]
+            return .requestParameters(body)
+        case .requestSignIn(let email, let pw):
+            let body: [String : Any] = [
+                "email": email,
+                "password": pw
+            ]
+            return .requestParameters(body)
+        }
+    }
+}
