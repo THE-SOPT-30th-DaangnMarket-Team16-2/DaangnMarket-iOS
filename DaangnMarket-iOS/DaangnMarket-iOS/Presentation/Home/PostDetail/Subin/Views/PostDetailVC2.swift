0e30088656fed32135ae8e1b93c290d760fb2444
SubinYoon <devxsby@gmail.com>
2022-05-25 11:14:49 +0900

    [Feat] #13- 프로젝트 레이아웃 1차 셋팅

---
+++ DaangnMarket-iOS/DaangnMarket-iOS/Presentation/Home/PostDetail/Subin/Views/PostDetailVC2.swift
@@ -0,0 +1,20 @@
+//
+//  PostDetailVC2.swift
+//  DaangnMarket-iOS
+//
+//  Created by subinyoon on 2022/05/25.
+//
+
+import UIKit
+
+class PostDetailVC2: UIViewController, Storyboarded {
+
+    // MARK: - Properties
+    static var storyboard: Storyboards = .postDetail2
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+}
