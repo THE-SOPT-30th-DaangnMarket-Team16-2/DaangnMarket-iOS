011be4e62442fe05632c5238302c9556fd0fb1ae
L-j-h-c <ckrgkswnsgh@naver.com>
2022-05-27 23:09:45 +0900

    [Chore] #21 - 샘플 데이터 수정

--- DaangnMarket-iOS/DaangnMarket-iOS/Domain/Model/PostListDataModel.swift
+++ DaangnMarket-iOS/DaangnMarket-iOS/Domain/Model/PostListDataModel.swift
@@ -24,8 +24,8 @@ extension PostListDataModel{
         PostListDataModel(productImageName: "productImageName00", productName: "의자", placeName: "경기도 성남시 분당구", price: "15,000원"),
         PostListDataModel(productImageName: "productImageName01", productName: "테이블", placeName: "경기도 용인시 수지구", price: "32,000원"),
         PostListDataModel(productImageName: "productImageName02", productName: "노트북", placeName: "경기도 수원시 팔달구", price: "32,000원"),
-        PostListDataModel(productImageName: "productImageName03", productName: "선풍기", placeName: "", price: "32,000원"),
-        PostListDataModel(productImageName: "productImageName04", productName: "데스크탑", placeName: "", price: "32,000원"),
-        PostListDataModel(productImageName: "productImageName05", productName: "귀걸이", placeName: "", price: "32,000원"),
-        PostListDataModel(productImageName: "productImageName06", productName: "노트북", placeName: "", price: "32,000원"),    ]
+        PostListDataModel(productImageName: "productImageName03", productName: "선풍기", placeName: "경기도 수원시 팔달구", price: "32,000원"),
+        PostListDataModel(productImageName: "productImageName00", productName: "데스크탑", placeName: "경기도 수원시 팔달구", price: "32,000원"),
+        PostListDataModel(productImageName: "productImageName01", productName: "귀걸이", placeName: "경기도 수원시 팔달구", price: "32,000원"),
+        PostListDataModel(productImageName: "productImageName02", productName: "노트북", placeName: "경기도 수원시 팔달구", price: "32,000원"),    ]
 }
