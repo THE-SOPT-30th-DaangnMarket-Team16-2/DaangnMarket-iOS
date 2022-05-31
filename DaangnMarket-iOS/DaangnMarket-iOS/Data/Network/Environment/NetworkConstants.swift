4d5043f3b70ea4ff9eac3116859d34ff5f118f02
L-j-h-c <ckrgkswnsgh@naver.com>
2022-05-31 16:56:23 +0900

    [Feat] #27 - 서비스 메서드 작성

--- DaangnMarket-iOS/DaangnMarket-iOS/Data/Network/Environment/NetworkConstants.swift
+++ DaangnMarket-iOS/DaangnMarket-iOS/Data/Network/Environment/NetworkConstants.swift
@@ -10,7 +10,8 @@ import Foundation
 struct URLConstants {
     
     // MARK: - Base URL
-    static let baseURL = "https://83008f6f-c9e8-4bb3-a1f8-db102f7e9b13.mock.pstmn.io"
+    static let baseURL = "http://13.125.157.62:8000"
+    static let mockingURL = "https://83008f6f-c9e8-4bb3-a1f8-db102f7e9b13.mock.pstmn.io"
 }
 
 struct NetworkEnvironment {
--- DaangnMarket-iOS/DaangnMarket-iOS/Data/Network/Routers/HomeRouter.swift
+++ DaangnMarket-iOS/DaangnMarket-iOS/Data/Network/Routers/HomeRouter.swift
@@ -10,6 +10,7 @@ import Alamofire
 
 enum HomeRouter {
     case getPostDetail(postId: String)
+    case changeSellStatus(postId: String, onSale: Int)
 }
 
 extension HomeRouter: BaseRouter {
@@ -18,20 +19,29 @@ extension HomeRouter: BaseRouter {
         switch self {
         case .getPostDetail(let postId):
             return "/feed/\(postId)"
+        case .changeSellStatus(_, _):
+            return "/feed/on-sale"
         }
     }
     
     var method: HTTPMethod {
         switch self {
-        default:
-            return .get
+        case .getPostDetail(_):
+                return .get
+        case .changeSellStatus(_, _):
+                return .put
         }
     }
     
     var parameters: RequestParams {
         switch self {
-        default:
+        case .getPostDetail(_):
             return .requestPlain
+        case .changeSellStatus(let postId, let onSale):
+            let body: [String : Any] =
+            ["id": postId,
+             "onSale": onSale ]
+            return .requestBody(body)
         }
     }
 }
