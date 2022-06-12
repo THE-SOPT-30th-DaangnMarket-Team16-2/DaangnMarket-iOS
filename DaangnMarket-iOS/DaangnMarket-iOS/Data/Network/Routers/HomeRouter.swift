9f113bd5d119209bb6a5d19c65d6a918331729c2
devxsby <devxsby@gmail.com>
2022-06-13 02:39:14 +0900

    [Feat] #41- List 조회 무한스크롤 GET API 서버 연동

--- DaangnMarket-iOS/DaangnMarket-iOS/Data/Network/Routers/HomeRouter.swift
+++ DaangnMarket-iOS/DaangnMarket-iOS/Data/Network/Routers/HomeRouter.swift
@@ -9,56 +9,61 @@ import Foundation
 import Alamofire
 
 enum HomeRouter {
-    case getPostDetail(postId: String)
-    case changeSellStatus(postId: String, onSale: String)
-    case changeLikeStatus(postId: String)
-    case createPostWrite(imageCount: Int, title: String, category: String, price: Int, contents: String, isPriceSuggestion: Bool)
     case getPostList
+    case getPostWithPage(page: Int, limit: Int)
+    case createPostWrite(imageCount: Int, title: String, category: String, price: Int, contents: String, isPriceSuggestion: Bool)
+    case getPostDetail(postId: String)
+    case changeLikeStatus(postId: String)
+    case changeSellStatus(postId: String, onSale: String)
 }
 
 extension HomeRouter: BaseRouter {
     
     var path: String {
         switch self {
-        case .getPostDetail(let postId):
-            return "/feed/\(postId)"
-        case .changeSellStatus(_, _):
-            return "/feed/on-sale"
-        case .changeLikeStatus(let postId):
-            return "/feed/like/\(postId)"
-        case .createPostWrite(_, _, _, _, _, _):
-            return "/feed"
         case .getPostList:
             return "/feed"
+        case .getPostWithPage:
+            return "/feed_page"
+        case .createPostWrite(_, _, _, _, _, _):
+            return "/feed"
+        case .getPostDetail(let postId):
+            return "/feed/\(postId)"
+        case .changeLikeStatus(let postId):
+            return "/feed/like/\(postId)"
+        case .changeSellStatus(_, _):
+            return "/feed/on-sale"
         }
     }
     
     var method: HTTPMethod {
         switch self {
-        case .getPostDetail(_):
-                return .get
-        case .changeSellStatus(_, _):
-                return .put
-        case .changeLikeStatus(_):
-            return .put
-        case .createPostWrite(_, _, _, _, _, _):
-            return .post
         case .getPostList:
             return .get
+        case .getPostWithPage(_, _):
+            return .get
+        case .createPostWrite(_, _, _, _, _, _):
+            return .post
+        case .getPostDetail(_):
+            return .get
+        case .changeLikeStatus(_):
+            return .put
+        case .changeSellStatus(_, _):
+            return .put
         }
     }
     
     var parameters: RequestParams {
         switch self {
-        case .getPostDetail(_):
+        case .getPostList:
             return .requestPlain
-        case .changeSellStatus(let postId, let onSale):
-            let body: [String : Any] =
-            ["id": postId,
-             "onSale": onSale ]
-            return .requestBody(body)
-        case .changeLikeStatus(_):
-            return .requestPlain
+            
+        case .getPostWithPage(let page, let limit):
+            let params: [String : Any] =
+            ["page" : page,
+             "limit" : limit]
+            return .query(params)
+            
         case .createPostWrite(let imageCount, let title, let category, let price, let contents, let isPriceSuggestion):
             let body: [String : Any] =
             ["imageCount" : imageCount,
@@ -69,8 +74,17 @@ extension HomeRouter: BaseRouter {
              "isPriceSuggestion" : isPriceSuggestion]
             return .requestBody(body)
             
-        case .getPostList:
+        case .getPostDetail(_):
             return .requestPlain
+
+        case .changeLikeStatus(_):
+            return .requestPlain
+            
+        case .changeSellStatus(let postId, let onSale):
+            let body: [String : Any] =
+            ["id": postId,
+             "onSale": onSale]
+            return .requestBody(body)
         }
     }
 }
--- DaangnMarket-iOS/DaangnMarket-iOS/Presentation/Home/PostList/Subin2/Views/PostListVC2.swift
+++ DaangnMarket-iOS/DaangnMarket-iOS/Presentation/Home/PostList/Subin2/Views/PostListVC2.swift
@@ -8,100 +8,144 @@
 import UIKit
 
 final class PostListVC2: UIViewController, Storyboarded {
-        
+    
+    // MARK: - Properties
     static var storyboard: Storyboards = .postList2
+    var newlistDataModel: [PostList] = []
+    var listDataModel: [PostList] = []
+    private var infiniteScrollPage: Int = 0
+    private var infiniteScrollLimit: Int = 10
+    private var isInfiniteScroll = true
+    private var listLastID: String?
     
-    private var model = ItemModel.samplePost
-    
-    private lazy var naviLocateButton: UIButton =  {
-        let bt = UIButton()
-        bt.setTitleColor(UIColor.carrotBlack, for: .normal)
-        bt.setTitle("잠실3동", for: .normal)
-        bt.titleLabel?.font = .NotoBold(size: 18)
-        return bt
-    }()
-    
-    private lazy var naviSearchButton: UIButton =  {
-        let bt = UIButton()
-        bt.setImage(ImageLiterals.PostList2.searchIcon, for: .normal)
-        return bt
-    }()
-    
-    private lazy var naviMenuButton: UIButton =  {
-        let bt = UIButton()
-        bt.setImage(ImageLiterals.PostList2.menuIcon, for: .normal)
-        return bt
-    }()
-    
-    private lazy var naviAlarmButton: UIButton =  {
-        let bt = UIButton()
-        bt.setImage(ImageLiterals.PostList2.alarmIcon, for: .normal)
-        return bt
-    }()
-
+    // MARK: - UI Component Part
+    @IBOutlet weak var plusBtn: UIButton!
     @IBOutlet weak var itemTableView: UITableView!
     @IBOutlet weak var floatingButton: UIButton!
     
+    // MARK: - Life Cycle Part
     override func viewDidLoad() {
         super.viewDidLoad()
         registerNib()
-        configUI()
+        navigationBarUI()
+        getPostWithPage(lastID: listLastID ?? "") {
+            self.isInfiniteScroll = true
+        }
     }
     
     override func viewWillAppear(_ animated: Bool) {
         setNavigationBar()
     }
     
-    private func registerNib() {
-        
-        let nib = UINib(nibName: PostTVC.identifier, bundle: nil)
-        itemTableView.register(nib, forCellReuseIdentifier: PostTVC.identifier)
-
-        itemTableView.delegate = self
-        itemTableView.dataSource = self
-    }
-    
+    // MARK: - IBAction Part
     @IBAction func floatingButtonTapped(_ sender: Any) {
         let writeVC = PostWriteVC.instantiate()
         self.navigationController?.pushViewController(writeVC, animated: true)
     }
     
-    private func configUI() {
-        let spacer = UIBarButtonItem(barButtonSystemItem: .fixedSpace, target: nil, action: nil)
-        spacer.width = 16
-                
-        self.navigationItem.setLeftBarButton(UIBarButtonItem(customView: naviLocateButton), animated: false)
-        self.navigationItem.rightBarButtonItems = [UIBarButtonItem(customView: naviAlarmButton), spacer, UIBarButtonItem(customView: naviMenuButton), spacer, UIBarButtonItem(customView: naviSearchButton)]
+    // MARK: - Custom Method Part
+    private func registerNib() {
+        let nib = UINib(nibName: PostTVC.identifier, bundle: nil)
+        itemTableView.register(nib, forCellReuseIdentifier: PostTVC.identifier)
+        itemTableView.delegate = self
+        itemTableView.dataSource = self
     }
     
     private func setNavigationBar() {
         self.navigationController?.isNavigationBarHidden = false
     }
-}
-
-extension PostListVC2: UITableViewDelegate{
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 140
+    
+    private func navigationBarUI() {
+        let naviLocateButton: UIButton =  {
+            let bt = UIButton()
+            bt.setTitleColor(UIColor.carrotBlack, for: .normal)
+            bt.setTitle("잠실3동", for: .normal)
+            bt.titleLabel?.font = .NotoBold(size: 18)
+            return bt
+        }()
+        
+        let naviSearchButton: UIButton =  {
+            let bt = UIButton()
+            bt.setImage(ImageLiterals.PostList2.searchIcon, for: .normal)
+            bt.frame = CGRect(x: 0, y: 0, width: 24, height: 24)
+            return bt
+        }()
+        
+        let naviMenuButton: UIButton =  {
+            let bt = UIButton()
+            bt.setImage(ImageLiterals.PostList2.menuIcon, for: .normal)
+            bt.frame = CGRect(x: 0, y: 0, width: 24, height: 24)
+            return bt
+        }()
+        
+        let naviAlarmButton: UIButton =  {
+            let bt = UIButton()
+            bt.setImage(ImageLiterals.PostList2.alarmIcon, for: .normal)
+            bt.frame = CGRect(x: 0, y: 0, width: 24, height: 24)
+            return bt
+        }()
+        
+        let spacer = UIBarButtonItem(barButtonSystemItem: .fixedSpace, target: nil, action: nil)
+        spacer.width = 16
+        
+        self.navigationItem.setLeftBarButton(UIBarButtonItem(customView: naviLocateButton), animated: false)
+        self.navigationItem.rightBarButtonItems = [UIBarButtonItem(customView: naviAlarmButton), spacer, UIBarButtonItem(customView: naviMenuButton), spacer, UIBarButtonItem(customView: naviSearchButton)]
     }
 }
 
-extension PostListVC2: UITableViewDataSource{
+// MARK: - UITableView Delegate, DataSource
+extension PostListVC2: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 140
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return model.count
+        return newlistDataModel.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: PostTVC.identifier) as? PostTVC else { return UITableViewCell() }
-        
-        cell.setData(itemData: model[indexPath.row])
+        cell.setData(self.newlistDataModel[indexPath.row])
         return cell
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        
-        tableView.deselectRow(at: indexPath, animated: true)
         let detailVC2 = PostDetailVC2.instantiate()
-        detailVC2.postId = "628f3743b32d474b28bba948"
+        detailVC2.postId = newlistDataModel[indexPath.row].id
         self.navigationController?.pushViewController(detailVC2, animated: true)
+    }
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        
+        if scrollView.contentOffset.y > scrollView.contentSize.height - scrollView.bounds.height {
+            if isInfiniteScroll {
+                isInfiniteScroll = false
+                
+                listLastID = listDataModel.last?.id
+                getPostWithPage(lastID: listLastID ?? "") {
+                    self.isInfiniteScroll = true
+                }
+            }
+        }
+    }
+}
+
+// MARK: - NETWORK
+extension PostListVC2 {
+    private func getPostWithPage(lastID: String, completion: @escaping() -> Void) {
+        HomeService.shared.getPostWithPage(page: infiniteScrollPage, limit: infiniteScrollLimit) { networkResult in
+            switch networkResult {
+            case .success(let data):
+                if let data = data as? [PostList] {
+                    self.newlistDataModel.append(contentsOf: data)
+                    self.infiniteScrollPage += 10
+                    self.itemTableView.reloadData()
+                }
+                completion()
+            default:
+                break;
+            }
+        }
     }
 }
